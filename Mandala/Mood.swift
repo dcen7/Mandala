efcecb164e597fc8618d3708f83fafacd69a993e
dcen7 <mdcengiz@gmail.com>
2022-02-24 07:21:44 -0600

    create model types and add resources

---
+++ Mandala/Mood.swift
@@ -0,0 +1,26 @@
+//
+//  Mood.swift
+//  Mandala
+//
+//  Created by Mehmet Deniz Cengiz on 2/18/22.
+//
+
+import UIKit
+
+struct Mood {
+    var name: String
+    var image: UIImage
+    var color: UIColor
+}
+
+extension Mood {
+    static let angry = Mood(name: "angry", image: UIImage(resource: .angry), color: UIColor.angry)
+    static let confused = Mood(name: "confused", image: UIImage(resource: .confused), color: UIColor.confused)
+    static let crying = Mood(name: "crying", image: UIImage(resource: .crying), color: UIColor.crying)
+    static let goofy = Mood(name: "goofy", image: UIImage(resource: .goofy), color: UIColor.goofy)
+    static let happy = Mood(name: "happy", image: UIImage(resource: .happy), color: UIColor.happy)
+    static let meh = Mood(name: "meh", image: UIImage(resource: .meh), color: UIColor.meh)
+    static let sad = Mood(name: "sad", image: UIImage(resource: .sad), color: UIColor.sad)
+    static let sleepy = Mood(name: "sleepy", image: UIImage(resource: .sleepy), color: UIColor.sleepy)
+    
+}
---
+++ Mandala/MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala
+//
+//  Created by Mehmet Deniz Cengiz on 2/18/22.
+//
+
+import Foundation
+
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+}
