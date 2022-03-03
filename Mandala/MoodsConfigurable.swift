3b42a2ae01aea1a079eeacae854902549b464abc
dcen7 <mdcengiz@gmail.com>
2022-03-03 09:10:38 -0600

    add protocol for add function, handle embed segue, add new mood entries

---
+++ Mandala/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by Mehmet Deniz Cengiz on 3/2/22.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
